cad9b09caf9efb346c8c2e94e6f688c2f84bc134
Pallavibale123 <81184356+Pallavibale123@users.noreply.github.com>
2025-08-28 22:29:44 -0600

    Update count_set_bits.sv

--- count_set_bits.sv
+++ count_set_bits.sv
@@ -61,3 +61,48 @@ module testbench;
     $finish;
   end
 endmodule
+
+/*
+count of ones in number 0 is 0
+count of ones in number 3 is 2
+count of ones in number 7 is 5
+count of ones in number 32 is 6
+*/
+
+//Q3. Count total number of set bits from number down to 1
+module testbench;
+  
+  function automatic int count_ones(input int unsigned num);
+    int max_count = 0;
+    for (int i = num; i > 0; i-- )begin
+      int count = 0;
+      int temp = i;
+      while (temp != 0 )begin
+        temp = (temp & (temp - 1));
+        count = count + 1;
+      end
+
+      max_count = max_count + count;
+    end
+    return max_count;
+  endfunction
+  
+  initial begin
+    int unsigned testing[] = '{0, 2, 3, 7, 32};
+    int unsigned result, num;
+    foreach(testing[i])begin
+      num = testing[i];
+      result = count_ones(num);
+      $display("count of ones from 1 to the number %0d is %0d", num, result);	
+    end
+    $finish;
+  end
+endmodule
+
+/* 
+count of ones from 1 to the number 0 is 0
+count of ones from 1 to the number 2 is 2
+count of ones from 1 to the number 3 is 4
+count of ones from 1 to the number 7 is 12
+count of ones from 1 to the number 32 is 81
+*/
