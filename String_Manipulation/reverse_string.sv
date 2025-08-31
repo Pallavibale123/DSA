30988f8434bc67017cba20ee5d89c1858ba343a2
Pallavibale123 <81184356+Pallavibale123@users.noreply.github.com>
2025-08-31 15:37:50 -0600

    Create reverse_string.sv

---
+++ String_Manipulation/reverse_string.sv
@@ -0,0 +1,27 @@
+// In SystemVerilog, functions only support pass-by-value arguments, so they can’t directly modify the caller’s variable.
+// If I want to reverse in place, I need ref, and only tasks allow ref arguments.
+// That’s why I used a task for in-place reverse, but a function for return-a-new-string reverse.
+
+//Q1. 1. **Reverse a string** Input: `"Hello"` → Output: `"olleH"`
+module testbench;
+  task automatic reverse_string(ref string str);
+    byte temp;
+    int i =0;
+    int j = str.len() - 1;
+
+    while(i < j)begin
+      temp = str[i];
+      str[i] = str[j];
+      str[j] = temp;
+      i += 1;
+      j -= 1;
+    end
+  endtask
+  
+  
+  initial begin
+    string s1 = "Hello";
+    reverse_string(s1);
+    $display("the reverse in place string is %s", s1);
+  end
+endmodule
