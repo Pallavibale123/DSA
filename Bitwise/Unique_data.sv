1f518b6f49fe15152af8a4f5699744dd6dd874b0
Pallavibale123 <81184356+Pallavibale123@users.noreply.github.com>
2025-09-03 22:55:25 -0600

    Update Unique_data.sv

--- Bitwise/Unique_data.sv
+++ Bitwise/Unique_data.sv
@@ -23,3 +23,31 @@ module testbench;
     $display("The unique data is %0d", data);
   end
  endmodule
+
+//Q2. Find two unique numbers where all others appear twice
+module testbench;
+  function void two_unique(input int signed arr[], output int signed u1, u2);
+    int signed xor_all = 0;
+    int signed right_set;
+    u1 = 0;
+    u2 = 0;
+    foreach (arr[i]) xor_all ^= arr[i];
+
+    right_set = xor_all & (-xor_all);
+
+    foreach(arr[i])begin
+      if(arr[i] & right_set) begin
+        u1 ^= arr[i];
+      end else
+        u2 ^= arr[i];
+    end
+  endfunction
+  
+  initial begin
+    int signed arr[] = '{0,1,-2,4,4,-2,1,0,8,-3};
+    int signed u1, u2;
+    two_unique(arr, u1, u2);
+    $display(" The two unique numbers are %0d %0d", u1, u2);
+    $finish;
+  end
+endmodule
