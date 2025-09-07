c27c15f502954a17dc2f50414b520e14a779e2b0
Pallavibale123 <81184356+Pallavibale123@users.noreply.github.com>
2025-09-06 19:52:55 -0600

    Update rotate_array.sv

--- Array/rotate_array.sv
+++ Array/rotate_array.sv
@@ -16,3 +16,22 @@ endtask
     $display("The rotate by k is %0p",rotated);
   end
 endmodule
+
+//Q2. Left Rotation by k (without reverse, using extra array)
+module testbench;
+task automatic left_rotate(input int arr[], input int k, output int rotated[]);
+  int n = arr.size();
+  rotated = new[n];
+  k = k % n;
+  for(int i = 0; i < n; i++)begin
+    rotated[i] = arr[(i + k) % n];
+  end
+endtask
+  
+  initial begin
+    int a[] = '{0,1,3,4,5,5,7};
+    int rotated[];
+    left_rotate(a, 4, rotated);
+    $display("rotated array is %0p", rotated);
+  end
+endmodule
