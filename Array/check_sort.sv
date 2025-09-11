3a0b76f90ddb0d8ff223cb19b7840cd7f0191292
Pallavibale123 <81184356+Pallavibale123@users.noreply.github.com>
2025-09-10 22:46:54 -0600

    Update check_sort.sv

--- Array/check_sort.sv
+++ Array/check_sort.sv
@@ -1,24 +1,26 @@
 module testbench;
-  
   function automatic int check_sort(input int unsigned a[]);
+    int asc =1;
+    int des = 1;
     int n = a.size();
-    bit as = 1;
-    bit b = 1;
-    if (n <= 1) return 3;
-    for(int i = 0; i < n-1; i++)begin
-      if(a[i] > a[i+1]) as = 0;
-      if(a[i] < a[i+1]) b = 0;
+    if (n < 2) return -1;
+    for(int i = 0; i < n-1; i++ )begin
+      if(a[i] > a[i+1]) asc = 0;
+      if(a[i] < a[i+1]) des = 0;
     end
-    if (as) return 0;
-    else if(b) return 1;
-    else return 2;
+    
+    if(asc) return 1;
+    else if(des)return 2;
+    else return 3;
   endfunction
- 
-  initial begin
-    int unsigned arr[] ='{ };
+  
+  initial begin 
+    int unsigned arr[] = '{5,4,5,6};
     int result;
     result = check_sort(arr);
-   
-    $display(" the result is %0d", result);
+    if (result == 1) $display("The array is asending");
+    if (result == 2) $display("The array is desending");
+    if (result == -1) $display("The array is not sufficient");
+    if (result == 3) $display("Can't check");
   end
 endmodule
